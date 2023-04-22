3c736f29145edfb14f82ebf48953876a426df472
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-04-22 12:24:46 -0400

    Refactors Square component to show 'X' when clicked

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
-const Square = ({ value }: {value: string}) => {
-  return <button className="square">{value}</button>;
+const Square = () => {
+  // useState hook holds an array with a value and a setter function, default value is set to null here
+  const [value, setValue] = useState(null);
+
+  let handleClick = () => {
+    // creates a handler function for the click event - when called, 'setValue' changes the 'value' to X
+    setValue("X");
+  };
+
+  return (
+    // adds the 'handleClick' function to the button, which is called when the button is clicked
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
